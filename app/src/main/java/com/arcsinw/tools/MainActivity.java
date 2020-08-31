9c5804174c590514a69e1c8d0c95a1bdcbc014cd
arcsinw <wangx86@live.com>
2020-08-31 21:31:36 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsinw/tools/MainActivity.java
@@ -0,0 +1,50 @@
+package com.arcsinw.tools;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openLauncherPicker(View view) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setClassName("android",
+                "com.android.internal.app.ResolverActivity");
+        startActivity(intent);
+
+    }
+
+    public void transparentStatusBar(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            Window window = MainActivity.this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    /**
+     * https://stackoverflow.com/questions/3856423/how-to-toggle-the-statusbar
+     * @param view
+     */
+    public void toggleStatusBar(View view) {
+        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.flags ^= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setAttributes(attrs);
+        Toast.makeText(this, "EEE", Toast.LENGTH_SHORT).show();
+    }
+}
